64fc9c8bf8f96a0b4a8077d756fd2135cb77b27e
EnzoFerreira <enzoraci21@gmail.com>
2025-08-05 21:16:32 -0300

    spinning smoke!

--- lib/smoke_roller.dart
+++ lib/smoke_roller.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
 
-class SmokeRoller extends StatefulWidget{
-
+class SmokeRoller extends StatefulWidget {
   const SmokeRoller({super.key});
 
   @override
@@ -12,25 +12,34 @@ class SmokeRoller extends StatefulWidget{
 }
 
 //_name the _ make the class private and invisible
-class _SmokeRollerState extends State<SmokeRoller>{
+class _SmokeRollerState extends State<SmokeRoller> {
   var smokeImage = 'assets/imgs/Weapon_smokegrenade.png';
 
-  void throwSmoke() {
-    setState(() {
-      smokeImage = 'assets/imgs/Weapon_smokegrenade2.png';
-      //FIX LATER AND ADD SOUND
-      //sleep(Duration(seconds: 1));
-      //smokeImage = 'assets/imgs/Weapon_smokegrenade.png';
-    });
+  final AudioPlayer audioplayer = AudioPlayer();
+  final String smokeSound = 'assets/audio/smoke.mp3';
+
+  void throwSmoke() async {
+      await audioplayer.play(AssetSource('audio/smoke.mp3'));
+      //funny fact, o assetSource coloca sem avisar um /assets/, duplicando o path lol
+      for (int i = 0; i < 8; i++) {
+        // Espera 500ms antes de trocar a imagem
+        await Future.delayed(Duration(milliseconds: 500));
+
+        setState(() {
+          smokeImage = (i % 2 == 0)
+          //aqui ele pega o numero da imagem, o resultado da divisao vê se ela é true or false hmm
+              ? 'assets/imgs/Weapon_smokegrenade.png'
+              : 'assets/imgs/Weapon_smokegrenade2.png';
+        });
+      }
   }
 
   @override
-  build(context){
+  build(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(
-            smokeImage, width: 300),
+        Image.asset(smokeImage, width: 300),
         SizedBox(height: 20),
         TextButton(
           onPressed: throwSmoke,
@@ -39,7 +48,9 @@ class _SmokeRollerState extends State<SmokeRoller>{
             foregroundColor: Colors.white,
             textStyle: const TextStyle(fontSize: 28),
           ),
-          child: Text('Throw smoke'),
+          child: Text('When the smoke stop spinning \n'
+              '                       RUSH')
+          ,
         ),
       ],
     );
